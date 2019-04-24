4439e95760618285f2fe516e786fc0bbfbf5f67a
Tom Kay <oridan82@gmail.com>
2019-04-24 18:13:18 +0100

    remove use

--- src/Decorators/AbstractDecorator.php
+++ src/Decorators/AbstractDecorator.php
@@ -2,7 +2,6 @@
 namespace Packaged\Form\Decorators;
 
 use Packaged\Form\Decorators\Interfaces\Decorator;
-use Packaged\Glimpse\Core\HtmlTag;
 use Packaged\Ui\Html\HtmlElement;
 
 abstract class AbstractDecorator extends HtmlElement implements Decorator
