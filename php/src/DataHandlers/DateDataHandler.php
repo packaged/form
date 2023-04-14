755f900a459334d5f8bb8eac17d8a6c38d29dd42
Kyle Essex <kylegavinessex@hotmail.com>
2023-04-14 12:21:08 +0100

    Add options example Fix date formatting

--- php/src/DataHandlers/DateDataHandler.php
+++ php/src/DataHandlers/DateDataHandler.php
@@ -6,11 +6,6 @@ use Packaged\Ui\Html\HtmlElement;
 
 class DateDataHandler extends TextDataHandler
 {
-  public function formatValue($value)
-  {
-    return $value === null ? null : strtotime($value);
-  }
-
   protected function _createBaseElement(): HtmlElement
   {
     return Input::create()->setType(Input::TYPE_DATE);
